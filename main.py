2cfbb48de3cd86173d50e434cb905df5ff80adf3
Fabri <fabrilennart@gmail.com>
2026-02-08 23:14:23 -0300

    Refactoring of main code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pymysql
 import configparser
-
+from loguru import logger
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -12,19 +12,26 @@ db_password = config['data_base']['password']
 db_port = int(config['data_base']['port'])
 db_name = config['data_base']['database']
 
-# do the conection
+connection = None  
 
 try:
+    logger.debug("Trying the connection")
     connection = pymysql.connect(
                  host=db_host,
                  port=db_port,
                  user=db_user,
                  password=db_password,
                  database=db_name)
-    print("connection was success")
+    logger.info("Connection successful")
+    
+    
 except pymysql.MySQLError as e:
-    print("DB connection failed:", e)
+    logger.error(f"DB connection failed: {e}")  
 
+finally:
+    if connection:
+        connection.close()
+        logger.info("Connection closed")
 
 if __name__ == "__main__":
     pass
